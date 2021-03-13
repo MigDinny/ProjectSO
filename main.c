45a75c321f48c400615ea57e955378925f2fda82
Miguel Dinis <mikepro2013@gmail.com>
2021-03-13 23:33:07 +0000

    Initial commit
    gitignore updated and main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+int main(int argc, char **argv) {
+
+    
+    printf("HELLO WORLD");
+}
