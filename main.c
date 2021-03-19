cd992e4f0e413960d696b12bf308dd25799e3813
Miguel Dinis <mikepro2013@gmail.com>
2021-03-19 17:51:01 +0000

    tasks.txt created and main.c improved with a few changes

--- main.c
+++ main.c
@@ -1,15 +1,41 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <wait.h>
-#include <stdlib.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
+/*
 
+    Operating Systems - Final Project
+
+    @author Miguel Dinis | 2019219010 | miguelbarroso@student.dei.uc.pt | github.com/MigDinny
+    @author Rodrigo Ferreira | 2019220060 | rferreira@student.dei.uc.pt | github.com/IronMan988
+
+
+
+*/
+
+/*
+    Include api libraries
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Include project libraries
+*/
+//#include "lib.h"
+
+
+/*
+    main function
+    run as: ./formula1 [config-filename] 
+*/
 int main(int argc, char **argv) {
 
-    for (int i = 0; i < 3; i++)
-        printf("%d\n", i);
+    if (argc != 2) {
+		printf("Wrong number of parameters\n");
+		exit(0);
+	}
+
+
+
+
+    FILE *configFile = fopen(argv[1], "r");
     
-    printf("HELLO WORLD");
+
 }
