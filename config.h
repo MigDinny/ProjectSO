3f4d20d53b005b0a4a099ad4934d98b492a3177a
Rodrigo Ferreira <76401919+IronMan988@users.noreply.github.com>
2021-03-21 16:27:53 +0000

    Config_load finished (#3)
    * config_load wip
    
    * load_config finished

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define MAX_CHAR_LINE 20
+#define MAX_LINES 6
 
 typedef struct config_t {
     int time;
@@ -12,5 +13,9 @@ typedef struct config_t {
     int tMaxBox;
     int fuelTank; 
 } config_t;
+
+void remove_spaces(char *);
+
+void check_commas(char *);
 
 int load_config(config_t*, char*);
